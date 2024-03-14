9f5f198664610c06633f530ab3fc97f379315b14
Ilya <ilya.balyasov@mail.ru>
2024-03-14 10:11:36 +0700

    Переделал анимацию появления

--- src/js/animation.js
+++ src/js/animation.js
@@ -1,9 +1,19 @@
 $(document).ready(function () {
 	setTimeout(function(){
+		$(".promo__title").removeClass("hidden");
+	}, 100);
+	setTimeout(function(){
+		$(".promo__subtitle").removeClass("hidden");
+	}, 200);
+	setTimeout(function(){
+		$(".promo__row").removeClass("hidden");
+	}, 300);
+	setTimeout(function(){
+		$(".promo__buttons").removeClass("hidden");
+	}, 400);
+	setTimeout(function(){
 		inView('.tops, .bottom, .left, .right').on('enter', function (event, isInView) {
 			$(event).addClass('viev-active');
-		}).on('exit', function (event, isInView) {
-			$(event).removeClass('viev-active');
 		});
 	}, 100);
 });
