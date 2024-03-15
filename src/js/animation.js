4e9d4820406ca3699785a7a60a5194c3504c61c9
Ilya <ilya.balyasov@mail.ru>
2024-03-15 09:43:15 +0700

    Убрал анимации, отсавил только в карточках

--- src/js/animation.js
+++ src/js/animation.js
@@ -1,18 +1,6 @@
 $(document).ready(function () {
 	setTimeout(function(){
-		$(".promo__title").removeClass("hidden");
-	}, 100);
-	setTimeout(function(){
-		$(".promo__subtitle").removeClass("hidden");
-	}, 200);
-	setTimeout(function(){
-		$(".promo__row").removeClass("hidden");
-	}, 300);
-	setTimeout(function(){
-		$(".promo__buttons").removeClass("hidden");
-	}, 400);
-	setTimeout(function(){
-		inView('.tops, .bottom, .left, .right').on('enter', function (event, isInView) {
+		inView('.bottom').on('enter', function (event, isInView) {
 			$(event).addClass('viev-active');
 		});
 	}, 100);
